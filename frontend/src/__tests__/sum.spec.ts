d7fb55781fda4a46ed06ebf866ed5f2f0124ce84
dev2820 <second28200@naver.com>
2023-11-13 18:31:43 +0900

    chore: eslint,prettier 셋업
    eslint
    - simple import sort
    - google style guide lint 설치
    - 일부 충돌하는 google 옵션 끔
    
    prettier
    - 이전에 논의한 대로 prettier 셋업함,
    
    alias
    - @=src 얼리어스 설정

--- frontend/src/__tests__/sum.spec.ts
+++ frontend/src/__tests__/sum.spec.ts
@@ -1,7 +1,7 @@
-import { expect, it } from "vitest";
+import { expect, it } from 'vitest';
 
 const sum = (a: number, b: number) => a + b;
 
-it("should be sum two values", () => {
+it('should be sum two values', () => {
   expect(sum(1, 2)).toBe(3);
 });
