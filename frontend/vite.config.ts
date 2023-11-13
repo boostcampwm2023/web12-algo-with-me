c3de782534dd13027d18b0aa302bb83ea5a913a7
dev2820 <second28200@naver.com>
2023-11-13 18:31:43 +0900

    chore: eslint,prettier 셋업
    eslint
    - simple import sort
    - google style guide lint 설치
    - 일부 충돌하는 google 옵션 끔
    
    prettier
    - 이전에 논의한 대로 prettier 셋업함,
    
    alias
    - @=src 얼리어스 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
+  },
 });
