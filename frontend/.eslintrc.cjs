c28e90c18893c881377cc850de6e37637f9aa17a
dev2820 <second28200@naver.com>
2023-11-13 18:31:43 +0900

    chore: eslint,prettier 셋업
    eslint
    - simple import sort
    - google style guide lint 설치
    - 일부 충돌하는 google 옵션 끔
    
    prettier
    - 이전에 논의한 대로 prettier 셋업함,
    
    alias
    - @=src 얼리어스 설정

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -5,14 +5,22 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'google',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['simple-import-sort', 'react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'object-curly-spacing': 'off',
+    'require-jsdoc': 'off',
+    'max-len': 'off',
+    indent: 'off',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [['^.+\\.s?css$'], ['^react'], ['^@(/.*|$)']],
+      },
     ],
   },
-}
+};
