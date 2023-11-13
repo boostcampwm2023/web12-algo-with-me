1ea76ba08ce6bfd06de365376843ebd0d9f1eccc
dev2820 <second28200@naver.com>
2023-11-13 22:08:08 +0900

    chore: pandaCSS setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,17 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: [
       {
         find: '@',
         replacement: path.resolve(__dirname, 'src'),
       },
+      {
+        find: '@style',
+        replacement: path.resolve(__dirname, 'styled-system'),
+      },
     ],
   },
+  plugins: [react()],
 });
