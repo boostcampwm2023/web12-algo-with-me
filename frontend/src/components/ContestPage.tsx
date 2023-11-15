6902fd3eee8fb8fd7277152f002640c6f56cedcd
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-11-15 23:17:31 +0900

    update: 리뷰 반영
    - ContestPage의 saveToLocalStorage함수와 clearLocalStorage함수를 제거했습니다.
    - ContestPage를 App.tsx에 추가했습니다.

--- frontend/src/components/ContestPage.tsx
+++ frontend/src/components/ContestPage.tsx
@@ -5,20 +5,6 @@ import Editor from './Editor';
 const ContestPage = () => {
   const [code, setCode] = useState(localStorage.getItem('myValue') || 'function solution() {\n\n}');
 
-  const saveToLocalStorage = () => {
-    // Editor에서 작성한 code를 로컬스토리지에 저장합니다.
-    localStorage.setItem('myValue', code);
-    console.log(code);
-    console.log('Code saved to localStorage');
-  };
-
-  const clearLocalStorage = () => {
-    // 로컬스토리지에 저장된 code를 지우고, 초기 solution함수를 지정해줍니다.
-    localStorage.removeItem('myValue');
-    console.log('Local Storage Cleared');
-    setCode('function solution() {\n\n}');
-  };
-
   return (
     <div>
       <Editor code={code} setCode={setCode} />
