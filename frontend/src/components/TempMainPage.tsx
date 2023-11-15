6842a0006a3e87367d02ae4c6f1b2d4fa077590f
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-11-15 20:55:01 +0900

    chore: 주석 추가

--- frontend/src/components/TempMainPage.tsx
+++ frontend/src/components/TempMainPage.tsx
@@ -6,16 +6,19 @@ const TempMainPage = () => {
   const [code, setCode] = useState(localStorage.getItem('myValue') || 'function solution() {\n\n}');
 
   const getCode = () => {
+    // Editor에서 작성한 code를 return해줍니다.
     return code;
   };
 
   const saveToLocalStorage = () => {
+    // Editor에서 작성한 code를 로컬스토리지에 저장합니다.
     localStorage.setItem('myValue', code);
     console.log(code);
     console.log('Code saved to localStorage');
   };
 
   const clearLocalStorage = () => {
+    // 로컬스토리지에 저장된 code를 지우고, 초기 solution함수를 지정해줍니다.
     localStorage.removeItem('myValue');
     console.log('Local Storage Cleared');
     setCode('function solution() {\n\n}');
