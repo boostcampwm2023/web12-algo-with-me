b2aef4477509fa8cb6b6e7da9ca477bcbd9e4c70
Yechan Lee <yechan24680@gmail.com>
2023-11-16 15:59:56 +0900

    feat: timeout 10초 추가

--- be/algo-with-me-docker/docker-sh/run.sh
+++ be/algo-with-me-docker/docker-sh/run.sh
@@ -1,4 +1,4 @@
-sudo docker run \
+sudo docker run -d \
 -p 3000:3000 \
 -e COMPETITION_ID=$1 \
 -e USER_ID=$2 \
@@ -6,7 +6,7 @@ sudo docker run \
 -v $HOME/algo-with-me/problems:/algo-with-me/problems:ro \
 -v $HOME/algo-with-me/testcases:/algo-with-me/testcases:ro \
 -v $HOME/algo-with-me/submissions:/algo-with-me/submissions \
---user $(id -u):$(id -g) \
+--user "$(id -u)":"$(id -g)" \
 --name algo-with-me-docker \
 algo-with-me-docker:latest
 
