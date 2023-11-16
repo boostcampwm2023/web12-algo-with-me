830a5e64d28542c7aada8dd4e1928d78420dac01
rladydgn <39542757+rladydgn@users.noreply.github.com>
2023-11-17 01:53:14 +0900

    fix: http request로 제출하던것을 websocket 제출로 변경

--- be/algo-with-me-api/src/competition/controllers/competition.controller.ts
+++ be/algo-with-me-api/src/competition/controllers/competition.controller.ts
@@ -1,6 +1,5 @@
-import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
-import { CreateSubmissionDto } from '../dto/create-submission.dto';
 import { CompetitionService } from '../services/competition.service';
 
 @Controller('competitions')
@@ -12,9 +11,9 @@ export class CompetitionController {
     return this.competitionService.findOneProblem(id);
   }
 
-  @Post('submissions')
-  @UsePipes(new ValidationPipe({ transform: true }))
-  createSubmission(@Body() createSubmissionDto: CreateSubmissionDto) {
-    return this.competitionService.scoreSubmission(createSubmissionDto);
-  }
+  // @Post('submissions')
+  // @UsePipes(new ValidationPipe({ transform: true }))
+  // createSubmission(@Body() createSubmissionDto: CreateSubmissionDto) {
+  //   return this.competitionService.scoreSubmission(createSubmissionDto);
+  // }
 }
