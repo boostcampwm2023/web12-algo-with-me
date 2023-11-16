87077e31f3be4afbe6fec80f424087805f088d05
Yechan Lee <yechan24680@gmail.com>
2023-11-16 21:44:32 +0900

    chore: redis bull 설정 완료

--- be/algo-with-me-score/src/app.module.ts
+++ be/algo-with-me-score/src/app.module.ts
@@ -1,14 +1,38 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
 
-import { ScoreController } from './score/score.controller';
-import { ScoreModule } from './score/score.module';
-import { ScoreService } from './score/score.service';
-import { typeORMConfig } from './typeorm.config';
+import * as process from 'node:process';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+dotenv.config();
 
 @Module({
-  imports: [ScoreModule, TypeOrmModule.forRoot(typeORMConfig)],
-  controllers: [ScoreController],
-  providers: [ScoreService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      entities: [],
+    }),
+    BullModule.forRoot({
+      redis: {
+        host: 'localhost',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'testQueue',
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
