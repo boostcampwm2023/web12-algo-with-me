84ef7773b16d4bf27f8d64218df673afebff5050
Yechan Lee <yechan24680@gmail.com>
2023-11-17 19:43:07 +0900

    chore: PR에 올라간 프론트엔드 커밋 삭제

--- frontend/panda.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from '@pandacss/dev';
-
-export default defineConfig({
-  preflight: true,
-  include: ['./src/**/*.{js,jsx,ts,tsx}'],
-  exclude: [],
-  outdir: 'styled-system',
-});
--- frontend/src/__tests__/sum.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, it } from 'vitest';
-
-const sum = (a: number, b: number) => a + b;
-
-it('should be sum two values', () => {
-  expect(sum(1, 2)).toBe(3);
-});
