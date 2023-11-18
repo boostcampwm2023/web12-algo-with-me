385a7a3dd07c5644345028d432b1fc855844fbbf
rladydgn <39542757+rladydgn@users.noreply.github.com>
2023-11-18 14:02:09 +0900

    feat: 테스트케이스 채점완료시 저장&클라이언트에게 전송하는 service 구현

--- be/algo-with-me-api/src/competition/services/competition.service.ts
+++ be/algo-with-me-api/src/competition/services/competition.service.ts
@@ -1,18 +1,24 @@
 import { InjectQueue } from '@nestjs/bull';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { WebSocketServer } from '@nestjs/websockets';
 import { Queue } from 'bull';
+import { Server } from 'socket.io';
 import { Repository } from 'typeorm';
 
 import { existsSync, readFileSync } from 'fs';
 import * as path from 'path';
 
 import { CreateSubmissionDto } from '../dto/create-submission.dto';
+import { ScoreResultDto } from '../dto/score-result.dto';
 import { Problem } from '../entities/problem.entity';
 import { Submission } from '../entities/submission.entity';
 
 @Injectable()
 export class CompetitionService {
+  @WebSocketServer()
+  server: Server;
+
   constructor(
     @InjectRepository(Problem) private readonly problemRepository: Repository<Problem>,
     @InjectRepository(Submission) private readonly submissionRepository: Repository<Submission>,
@@ -50,5 +56,24 @@ export class CompetitionService {
     });
 
     return savedSubmission;
+  }
+
+  async saveScoreResult(scoreResultDto: ScoreResultDto) {
+    const submission = await this.submissionRepository.findOneBy({
+      id: scoreResultDto.submissionId,
+    });
+    if (!submission) throw new NotFoundException('제출 기록이 없습니다.');
+
+    const result = {
+      testcaseId: scoreResultDto.testcaseId,
+      result: scoreResultDto.result,
+      stdOut: scoreResultDto.stdOut,
+    };
+
+    submission.detail.push(result);
+    this.submissionRepository.save(submission);
+
+    result['problemId'] = scoreResultDto.problemId;
+    this.server.to(scoreResultDto.socketId).emit("message", result);
   }
 }
