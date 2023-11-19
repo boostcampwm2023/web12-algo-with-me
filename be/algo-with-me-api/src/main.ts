a0e9bf544f41426ba8e8b91c61831f26b9047c4b
rladydgn <39542757+rladydgn@users.noreply.github.com>
2023-11-19 15:02:25 +0900

    feat: swagger 구현

--- be/algo-with-me-api/src/main.ts
+++ be/algo-with-me-api/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from '@src/app.module';
 
+function setSwagger(app) {
+  const config = new DocumentBuilder()
+    .setTitle('algo-with-me-api')
+    .setDescription('algo with me API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+}
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
