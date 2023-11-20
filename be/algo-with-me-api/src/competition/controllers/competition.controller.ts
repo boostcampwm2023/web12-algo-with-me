0adc7ab7e0271984cce8edbdb181898aa07bbce7
rladydgn <39542757+rladydgn@users.noreply.github.com>
2023-11-20 15:08:57 +0900

    대회 controller에 swagger 적용:

--- be/algo-with-me-api/src/competition/controllers/competition.controller.ts
+++ be/algo-with-me-api/src/competition/controllers/competition.controller.ts
@@ -1,20 +1,27 @@
 import { Body, Controller, Get, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+import { CompetitionProblemResponseDto } from '../dto/competition.problem.response.dto';
 import { ScoreResultDto } from '../dto/score-result.dto';
 import { CompetitionService } from '../services/competition.service';
 
-@ApiTags('competitions')
+@ApiTags('대회(competitions)')
 @Controller('competitions')
 export class CompetitionController {
   constructor(private readonly competitionService: CompetitionService) {}
 
   @Get('problems/:id')
+  @ApiOperation({ summary: '대회 문제 상세 조회' })
+  @ApiResponse({ type: CompetitionProblemResponseDto })
   findOne(@Param('id') id: number) {
     return this.competitionService.findOneProblem(id);
   }
 
   @Post('scores')
+  @ApiOperation({
+    summary: '채점 서버에서 채점 완료시 요청받을 api',
+    description: '채점 서버에서 테스트케이스 별로 채점이 완료될경우 호출할 api',
+  })
   @UsePipes(new ValidationPipe({ transform: true }))
   saveScoreResult(@Body() scoreResultDto: ScoreResultDto) {
     this.competitionService.saveScoreResult(scoreResultDto);
