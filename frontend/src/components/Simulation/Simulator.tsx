3c1a60e4709b696a247a3d80e176d0674ee5725d
dev2820 <second28200@naver.com>
2023-11-21 18:18:50 +0900

    update: 테스트 입력 화면과 테스트 결과 화면 분리

--- frontend/src/components/Simulation/Simulator.tsx
+++ frontend/src/components/Simulation/Simulator.tsx
@@ -4,12 +4,11 @@ import type { ChangeEvent, HTMLAttributes } from 'react';
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   param: string;
-  result: unknown;
   onChangeParam: (newParam: string) => void;
 }
 
 export default function Simulator(props: Props) {
-  const { param, result, onChangeParam } = props;
+  const { param, onChangeParam } = props;
 
   const handleParamChange = (e: ChangeEvent<HTMLInputElement>) => {
     const newParam = e.target.value;
@@ -19,7 +18,6 @@ export default function Simulator(props: Props) {
   return (
     <div>
       <input className={inputStyle} onChange={handleParamChange} value={param}></input>
-      <p>result: {String(result)}</p>
     </div>
   );
 }
