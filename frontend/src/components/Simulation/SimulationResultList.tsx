fd47f657e282b85f83b34beb4443459af7c00105
dev2820 <second28200@naver.com>
2023-11-21 19:35:12 +0900

    refactor: 불필요한 컴포넌트 분리 제거

--- frontend/src/components/Simulation/SimulationResultList.tsx
+++ frontend/src/components/Simulation/SimulationResultList.tsx
@@ -6,10 +6,6 @@ interface Props extends HTMLAttributes<HTMLUListElement> {
   resultList: SimulationResult[];
 }
 
-interface SimulResultProps {
-  result: SimulationResult;
-}
-
 export function SimulationResultList(props: Props) {
   const { resultList = [] } = props;
 
@@ -17,21 +13,13 @@ export function SimulationResultList(props: Props) {
     <ul>
       {resultList.map((result) => (
         <li key={result.id}>
-          <SimulResult result={result}></SimulResult>
+          <div>
+            <p>입력: {result.input}</p>
+            <p>결과: {String(result.output)}</p>
+            <hr />
+          </div>
         </li>
       ))}
     </ul>
   );
 }
-
-const SimulResult = (props: SimulResultProps) => {
-  const { result } = props;
-
-  return (
-    <div>
-      <p>입력: {result.input}</p>
-      <p>결과: {String(result.output)}</p>
-      <hr />
-    </div>
-  );
-};
