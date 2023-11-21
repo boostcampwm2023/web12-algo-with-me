e722741e896d10e63c09ae10597afc564a8c6c6e
dev2820 <second28200@naver.com>
2023-11-21 12:04:56 +0900

    update: eval을 quickjs로 변경

---
+++ frontend/src/modules/evaluator/quickjs.ts
@@ -0,0 +1,49 @@
+import type { QuickJSContext, QuickJSWASMModule } from 'quickjs-emscripten';
+import { getQuickJS } from 'quickjs-emscripten';
+
+const MEM_LIMIT = 1024 * 1024; // 1GB
+const STACK_LIMIT = 1024 * 512; // 500MB
+
+export async function evaluate(code: string, params: string) {
+  const QuickJS = await getQuickJS();
+  const runtime = createRuntime(QuickJS);
+  const vm = runtime.newContext();
+
+  try {
+    return evalCode(vm, code, params);
+  } finally {
+    vm.dispose();
+    runtime.dispose();
+  }
+}
+
+const createRuntime = (quickjs: QuickJSWASMModule) => {
+  const runtime = quickjs.newRuntime();
+  runtime.setMemoryLimit(MEM_LIMIT);
+  runtime.setMaxStackSize(STACK_LIMIT);
+
+  return runtime;
+};
+
+const evalCode = (vm: QuickJSContext, code: string, params: string) => {
+  const script = wrapCodeWithTemplate(code, params);
+
+  const startTime = performance.now();
+  const result = vm.unwrapResult(vm.evalCode(script));
+  const endTime = performance.now();
+  const value = vm.dump(result);
+
+  result.dispose();
+
+  return {
+    time: endTime - startTime,
+    result: value,
+  };
+};
+
+const wrapCodeWithTemplate = (code: string, params: string) => {
+  return `
+    ${code}\n
+    solution(${params});
+  `;
+};
