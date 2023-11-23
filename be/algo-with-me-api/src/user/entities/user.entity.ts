bc751308cf4673b53aeb55825df9fa6d36cf2368
rladydgn <39542757+rladydgn@users.noreply.github.com>
2023-11-23 14:21:53 +0900

    fix: 엔티티에 있는 swagger 데코레이터 모두 삭제:

--- be/algo-with-me-api/src/user/entities/user.entity.ts
+++ be/algo-with-me-api/src/user/entities/user.entity.ts
@@ -1,4 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail } from 'class-validator';
 import {
   Column,
@@ -10,24 +9,19 @@ import {
 
 @Entity()
 export class User {
-  @ApiProperty({ description: '유저 id' })
   @PrimaryGeneratedColumn()
   id: number;
 
-  @ApiProperty({ description: '이메일' })
   @Column()
   @IsEmail()
   email: string;
 
-  @ApiProperty({ description: '닉네임 초기값은 이메일' })
   @Column()
   nickname: string;
 
-  @ApiProperty({ description: '생성일' })
   @CreateDateColumn()
   createdAt: Date;
 
-  @ApiProperty({ description: '수정일' })
   @UpdateDateColumn()
   updatedAt: Date;
 }
