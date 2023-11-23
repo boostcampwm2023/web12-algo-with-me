2e34d45d34f0bef5dcb9f28b83e49331a33ee37a
Yechan Lee <yechan24680@gmail.com>
2023-11-24 00:28:41 +0900

    chore: test 완료. 프레임코드 작성 완료. (백로그 참고)

--- be/algo-with-me-docker/docker-sh/run.sh
+++ be/algo-with-me-docker/docker-sh/run.sh
@@ -4,8 +4,8 @@ sudo docker run -d \
 -e USER_ID=$2 \
 -e PROBLEM_ID=$3 \
 -e TESTCASE_ID=$4 \
--v $HOME/algo-with-me/problems:/algo-with-me/problems:ro \
--v $HOME/algo-with-me/testcases:/algo-with-me/testcases:ro \
+-v $HOME/algo-with-me/problems:/algo-with-me/problems \
+-v $HOME/algo-with-me/testcases:/algo-with-me/testcases \
 -v $HOME/algo-with-me/submissions:/algo-with-me/submissions \
 --user "$(id -u)":"$(id -g)" \
 --name algo-with-me-docker \
