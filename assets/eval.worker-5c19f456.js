d369fe763899d553f6e083bfe3915c16eb682c5a
youseock <mahwin7085@gmail.com>
2023-11-23 16:14:44 +0900

    Updates

--- assets/eval.worker-5c19f456.js
+++
@@ -1,2 +0,0 @@
-(function(){"use strict";function evalCode(code,params){const result=eval(`${code}
-solution(${params})`);return{result}}self.addEventListener("message",async function(s){const t=s.data;try{const{code:e,param:a}=t,o=evalCode(e,a);self.postMessage(o)}catch(e){self.postMessage(e)}})})();
