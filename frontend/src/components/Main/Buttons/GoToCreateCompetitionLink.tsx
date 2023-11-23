c9569ad94a09daa2d1e734931fa3f286ca61a980
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-11-23 21:37:20 +0900

    feat: 로그인 여부에 따른 처리 로직 추가

--- frontend/src/components/Main/Buttons/GoToCreateCompetitionLink.tsx
+++ frontend/src/components/Main/Buttons/GoToCreateCompetitionLink.tsx
@@ -1,11 +1,20 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+const TOKEN_KEY = 'accessToken';
 
 export default function GoToCreateCompetitionLink() {
-  // TODO: 로그인 여부에 따른 페이지 이동 설정
+  const queryParams = new URLSearchParams(location.search);
+  const token = queryParams.get(TOKEN_KEY) || localStorage.getItem(TOKEN_KEY);
+  const navigate = useNavigate();
 
-  return (
-    <Link to="/contest/create">
-      <button>대회 생성</button>
-    </Link>
-  );
+  const handleNavigate = () => {
+    if (!token) {
+      alert('로그인이 필요합니다.');
+      navigate('/login');
+    } else {
+      navigate('/contest/create');
+    }
+  };
+
+  return <button onClick={handleNavigate}>대회 생성</button>;
 }
--- frontend/src/components/Main/Buttons/JoinCompetitionButton.tsx
+++ frontend/src/components/Main/Buttons/JoinCompetitionButton.tsx
@@ -1,22 +1,42 @@
 import axios from 'axios';
 
+const TOKEN_KEY = 'accessToken';
+
 export default function JoinCompetitionButton(props: { id: number }) {
   const apiUrl = import.meta.env.VITE_API_URL;
+
+  const queryParams = new URLSearchParams(location.search);
+  const token = queryParams.get(TOKEN_KEY) || localStorage.getItem(TOKEN_KEY);
+
   const handleJoinClick = async () => {
+    if (!token) {
+      alert('로그인이 필요합니다.');
+      window.location.href = '/login';
+      return;
+    }
+
     try {
-      await axios.post(`${apiUrl}/competitions/${props.id}`);
+      await axios.post(`${apiUrl}/competitions/${props.id}/participations`);
 
       alert('대회에 성공적으로 참여했습니다.');
       window.location.reload();
     } catch (error: unknown) {
-      console.error('Error joining competition:', error);
+      let errorMessage = 'Unexpected error occurred.';
 
-      if (axios.isAxiosError(error) && error.response && error.response.status === 403) {
-        alert('대회 참여에 실패했습니다. 서버에서 거절되었습니다.');
-        window.location.reload();
-      } else {
-        console.error('Unexpected error:', error);
+      if (axios.isAxiosError(error)) {
+        if (error.response) {
+          if (error.response.status === 403) {
+            errorMessage = '대회 참여에 실패했습니다. 서버에서 거절되었습니다.';
+          } else {
+            errorMessage = `HTTP Error ${error.response.status}`;
+          }
+        } else {
+          errorMessage = 'Network error occurred.';
+        }
       }
+
+      alert(errorMessage);
+      window.location.reload();
     }
   };
 
