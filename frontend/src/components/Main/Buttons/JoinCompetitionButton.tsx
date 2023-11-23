5253812b0582e3695df31325e96cfc89931007fd
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-11-23 19:09:39 +0900

    feat: 대회에 참여하는 로직 작성

--- frontend/src/components/Main/Buttons/JoinCompetitionButton.tsx
+++ frontend/src/components/Main/Buttons/JoinCompetitionButton.tsx
@@ -1,4 +1,24 @@
+import axios from 'axios';
+
 export default function JoinCompetitionButton(props: { id: number }) {
-  // TODO: 대회에 참여하는 로직 작성 / 참여하기 활성화 로직 작성
-  return <button>참여하기{props.id}</button>;
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const handleJoinClick = async () => {
+    try {
+      await axios.post(`${apiUrl}/competitions/${props.id}`);
+
+      alert('대회에 성공적으로 참여했습니다.');
+      window.location.reload();
+    } catch (error: unknown) {
+      console.error('Error joining competition:', error);
+
+      if (axios.isAxiosError(error) && error.response && error.response.status === 403) {
+        alert('대회 참여에 실패했습니다. 서버에서 거절되었습니다.');
+        window.location.reload();
+      } else {
+        console.error('Unexpected error:', error);
+      }
+    }
+  };
+
+  return <button onClick={handleJoinClick}>참여하기</button>;
 }
