126cbd085a86fcfc1e7692bdc3822b4de2741971
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-11-24 00:31:19 +0900

    feat: 이미 참여한 대회에 대해 참여하기 시도시 이미 참여한 대회라는 alert 표시

--- frontend/src/components/Main/Buttons/JoinCompetitionButton.tsx
+++ frontend/src/components/Main/Buttons/JoinCompetitionButton.tsx
@@ -35,6 +35,8 @@ export default function JoinCompetitionButton(props: { id: number }) {
         if (error.response) {
           if (error.response.status === 403) {
             errorMessage = '대회 참여에 실패했습니다. 서버에서 거절되었습니다.';
+          } else if (error.response.status === 400) {
+            errorMessage = '이미 참여한 대회입니다.';
           } else {
             errorMessage = `HTTP Error ${error.response.status}`;
           }
