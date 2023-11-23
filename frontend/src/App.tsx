e673300e74debb507622456ffcbf3133cdfa5743
dev2820 <second28200@naver.com>
2023-11-23 18:50:49 +0900

    refactor: msw 사용 로직 임시 제거 (현재는 socket으로 통신함)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,5 @@
 import { Outlet } from 'react-router-dom';
 
-// import worker from './__mocks__';
-
-// if (process.env.NODE_ENV === 'development') {
-//   worker.start();
-// }
-
 function App() {
   return <Outlet></Outlet>;
 }
