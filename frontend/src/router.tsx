841d8fde5d4c33435ed496a48ce4c1a85d180175
youseock <mahwin7085@gmail.com>
2023-11-23 10:53:52 +0900

    chore: 라우팅 추가

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,26 +1,31 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import Login from '@/components/Login';
 import ContestPage from '@/pages/ContestPage';
 import ProblemPage from '@/pages/ProblemPage';
 
 import App from './App';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        path: '/contest/:id',
-        element: <ContestPage />,
-      },
-      {
-        path: '/problem/:id',
-        element: <ProblemPage />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          index: true,
+          path: '/contest/:id',
+          element: <ContestPage />,
+        },
+        {
+          path: '/problem/:id',
+          element: <ProblemPage />,
+        },
+        { path: '/login', element: <Login /> },
+      ],
+    },
+  ],
+  { basename: process.env.NODE_ENV === 'production' ? '/web12-algo-with-me' : '' },
+);
 
 export default router;
