767241e55ed8a4efd0405cc427cf4b6580080908
rladydgn <39542757+rladydgn@users.noreply.github.com>
2023-11-23 14:21:53 +0900

    fix: 엔티티에 있는 swagger 데코레이터 모두 삭제:

--- be/algo-with-me-api/src/competition/entities/competition.entity.ts
+++ be/algo-with-me-api/src/competition/entities/competition.entity.ts
@@ -1,4 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger';
 import {
   Column,
   CreateDateColumn,
@@ -14,42 +13,32 @@ import { Submission } from './submission.entity';
 @Entity()
 export class Competition {
   @PrimaryGeneratedColumn()
-  @ApiProperty({ description: '대회 id' })
   id: number;
 
-  @ApiProperty({ description: '대회 이름' })
   @Column()
   name: string;
 
-  @ApiProperty({ description: '대회에 대한 설명글' })
   @Column('text')
   detail: string;
 
-  @ApiProperty({ description: '대회에 참여 가능한 최대 인원' })
   @Column()
   maxParticipants: number;
 
-  @ApiProperty({ description: '대회 시작 일시' })
   @Column()
   startsAt: Date;
 
-  @ApiProperty({ description: '대회 종료 일시' })
   @Column()
   endsAt: Date;
 
-  @ApiProperty({ description: '제출(submission) 테이블과 일대다 관계' })
   @OneToMany(() => Submission, (submission) => submission.competition)
   submissions: Submission[];
 
-  @ApiProperty({ description: '대회문제(competitionProblem) 테이블과 일대다 관계' })
   @OneToMany(() => CompetitionProblem, (competitionProblem) => competitionProblem.competition)
   competitionProblems: CompetitionProblem[];
 
-  @ApiProperty({ description: '레코드 생성 일시' })
   @CreateDateColumn()
   createdAt: Date;
 
-  @ApiProperty({ description: '레코드 수정 일시' })
   @UpdateDateColumn()
   updatedAt: Date;
 }
--- be/algo-with-me-api/src/competition/entities/competition.problem.entity.ts
+++ be/algo-with-me-api/src/competition/entities/competition.problem.entity.ts
@@ -1,4 +1,3 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 import { Competition } from './competition.entity';
@@ -7,14 +6,11 @@ import { Problem } from './problem.entity';
 @Entity()
 export class CompetitionProblem {
   @PrimaryGeneratedColumn()
-  @ApiProperty({ description: 'id' })
   id: number;
 
-  @ApiProperty({ description: '대회(competition) 테이블과 다대일 관계' })
   @ManyToOne(() => Competition, (competition) => competition.competitionProblems)
   competition: Competition;
 
-  @ApiProperty({ description: '문제(problem) 테이블과 다대일 관계' })
   @ManyToOne(() => Problem, (problem) => problem.competitionProblems)
   problem: Problem;
 
