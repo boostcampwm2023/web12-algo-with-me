f5a984d7db4a95672d742f138d22d21fd012a8b7
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-11-24 01:28:59 +0900

    refactor: ifelse문에서 switch문으로 리팩토링

--- frontend/src/apis/joinCompetition/index.ts
+++ frontend/src/apis/joinCompetition/index.ts
@@ -22,12 +22,16 @@ export async function joinCompetition(data: CompetitionApiData) {
 
     if (axios.isAxiosError(error)) {
       if (error.response) {
-        if (error.response.status === 403) {
-          errorMessage = '대회 참여에 실패했습니다. 서버에서 거절되었습니다.';
-        } else if (error.response.status === 400) {
-          errorMessage = '이미 참여한 대회입니다.';
-        } else {
-          errorMessage = `HTTP Error ${error.response.status}`;
+        switch (error.response.status) {
+          case 403:
+            errorMessage = '대회 참여에 실패했습니다. 서버에서 거절되었습니다.';
+            break;
+          case 400:
+            errorMessage = '이미 참여한 대회입니다.';
+            break;
+          default:
+            errorMessage = `HTTP Error ${error.response.status}`;
+            break;
         }
       } else {
         errorMessage = 'Network error occurred.';
