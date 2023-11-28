62dbf9ab0b141e44e74aba59b8ca346214f57fc8
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-11-28 13:45:55 +0900

    refactor: 에러 핸들링 코드 수정 및 상수 추가

--- frontend/src/apis/joinCompetition/index.ts
+++ frontend/src/apis/joinCompetition/index.ts
@@ -5,6 +5,7 @@ import axios from 'axios';
 
 const STATUS = {
   Forbidden: 403,
+  BadRequest: 400,
 } as const;
 
 export async function joinCompetition(data: CompetitionApiData) {
@@ -25,22 +26,24 @@ export async function joinCompetition(data: CompetitionApiData) {
   } catch (error: unknown) {
     let errorMessage = 'Unexpected error occurred.';
 
-    if (axios.isAxiosError(error)) {
-      if (error.response) {
-        switch (error.response.status) {
-          case STATUS.Forbidden:
-            errorMessage = '대회 참여에 실패했습니다. 서버에서 거절되었습니다.';
-            break;
-          case 400:
-            errorMessage = '이미 참여한 대회입니다.';
-            break;
-          default:
-            errorMessage = `HTTP Error ${error.response.status}`;
-            break;
-        }
-      } else {
-        errorMessage = 'Network error occurred.';
-      }
+    if (!axios.isAxiosError(error)) {
+      return 'Unexpected error occurred';
+    }
+
+    if (!error.response) {
+      return 'Network error occurred';
+    }
+
+    switch (error.response.status) {
+      case STATUS.Forbidden:
+        errorMessage = '대회 참여에 실패했습니다. 서버에서 거절되었습니다.';
+        break;
+      case STATUS.BadRequest:
+        errorMessage = '이미 참여한 대회입니다.';
+        break;
+      default:
+        errorMessage = `HTTP Error ${error.response.status}`;
+        break;
     }
 
     return errorMessage;
