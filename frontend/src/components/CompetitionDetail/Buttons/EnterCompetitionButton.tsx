f8439ef06ee315cde2dac69979cd0233faab7ed3
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-11-28 22:55:15 +0900

    update: 끝난 대회는 입장 못 하게 하는 로직 추가작성

--- frontend/src/components/CompetitionDetail/Buttons/EnterCompetitionButton.tsx
+++ frontend/src/components/CompetitionDetail/Buttons/EnterCompetitionButton.tsx
@@ -5,20 +5,25 @@ import useAuth from '@/hooks/login/useAuth';
 interface Props {
   id: number;
   startsAt: Date;
+  endsAt: Date;
 }
 
-export default function EnterCompetitionButton({ id, startsAt }: Props) {
-  const currentTime = new Date();
+export default function EnterCompetitionButton({ id, startsAt, endsAt }: Props) {
   const competitionLink = `/contest/${id}`;
   const { isLoggedin } = useAuth();
   const navigate = useNavigate();
 
   const handleNavigate = () => {
+    const currentTime = new Date();
+
     if (!isLoggedin) {
       alert('로그인이 필요합니다.');
       navigate('/login');
     } else if (currentTime < startsAt) {
       alert('아직 대회가 시작되지 않았습니다. 다시 시도해주세요');
+      window.location.reload();
+    } else if (currentTime >= endsAt) {
+      alert('해당 대회는 종료되었습니다.');
       window.location.reload();
     } else {
       navigate(competitionLink);
