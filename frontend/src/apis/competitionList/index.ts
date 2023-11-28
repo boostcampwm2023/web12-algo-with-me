3c26b7fc34d5b686653b680a232d433db5859aff
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-11-28 14:20:05 +0900

    refactor: api요청 코드 api모듈 사용

--- frontend/src/apis/competitionList/index.ts
+++ frontend/src/apis/competitionList/index.ts
@@ -2,11 +2,9 @@ import api from '@/utils/api';
 
 import { Competition } from './types';
 
-const apiUrl = import.meta.env.VITE_API_URL;
-
 export const fetchCompetitionList = async (): Promise<Competition[]> => {
   try {
-    const response = await api.get<Competition[]>(`${apiUrl}competitions`);
+    const response = await api.get('/competitions');
     return response.data;
   } catch (error) {
     console.error('Error fetching competitions:', (error as Error).message);
