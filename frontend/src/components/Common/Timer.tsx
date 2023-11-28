be43eeb5c80cc744fe8b130a49996b1fa0602364
youseock <mahwin7085@gmail.com>
2023-11-28 19:58:45 +0900

    chore: Timer 공통 컴포넌트로 뺌

--- frontend/src/components/Common/Timer.tsx
+++ frontend/src/components/Common/Timer.tsx
@@ -2,7 +2,7 @@ import { css } from '@style/css';
 
 import Loading from '@/components/Common/Loading';
 import useTimer from '@/hooks/competition/useTimer';
-import { formatTimeFromMiliSeconds } from '@/utils/date';
+import { formatMilliSecond } from '@/utils/date';
 import type { Socket } from '@/utils/socket';
 
 interface Props {
@@ -14,14 +14,14 @@ interface Props {
 export default function Timer(props: Props) {
   let { socket, endsAt, isConnected } = props;
   // api 연결이 X endsAt 대신 임시로 만들어놓은 것.
-  endsAt = new Date('2023-11-28T13:10:10.000Z');
+  endsAt = new Date('2023-11-29T13:10:10.000Z');
   const { remainMiliSeconds } = useTimer({ socket, endsAt });
 
   return (
     <section className={wrapperStyle}>
       <div>
         {isConnected && remainMiliSeconds !== -1 ? (
-          <span className={timeTextStyle}>{formatTimeFromMiliSeconds(remainMiliSeconds)}</span>
+          <span className={timeTextStyle}>{formatMilliSecond(remainMiliSeconds, 'hh:mm:ss')}</span>
         ) : (
           <section className={loadingBoxStyle}>
             <span className={disConnectedStyle}>연결 중...</span>
