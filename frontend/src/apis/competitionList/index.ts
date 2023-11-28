16079ddd7018ad865935bd6c6d25e0ed6843218b
dev2820 <second28200@naver.com>
2023-11-28 18:57:37 +0900

    chore: error 추가요

--- frontend/src/apis/competitionList/index.ts
+++ frontend/src/apis/competitionList/index.ts
@@ -5,9 +5,9 @@ import { Competition } from './types';
 export const fetchCompetitionList = async (): Promise<Competition[]> => {
   try {
     const response = await api.get('/competitions');
-    console.log(response);
     return response.data;
   } catch (error) {
+    console.log(error);
     console.error('Error fetching competitions:', (error as Error).message);
     throw error;
   }
