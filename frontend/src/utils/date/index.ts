7dc0fb304e1616f9f8821e4a6de8c4d4518288f2
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-11-29 14:44:47 +0900

    feat: formatKoreanDateTime이 'yyyy.mm.dd. hh:mm'을 받아도 반환할 수 있도록 코드 추가

--- frontend/src/utils/date/index.ts
+++ frontend/src/utils/date/index.ts
@@ -32,13 +32,18 @@ export const formatMilliSecond = (ms: number, form: string) => {
   return '';
 };
 
-export const formatKoreanDateTime = (date: Date) => {
-  return date.toLocaleString('ko-KR', {
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: false,
-  });
+export const formatKoreanDateTime = (dateString: string | number | Date) => {
+  const date = new Date(dateString);
+  if (isNaN(date.getTime())) {
+    // Invalid date, handle error or return default value
+    return 'Invalid Date';
+  }
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+
+  return `${year}. ${month}. ${day}. ${hours}:${minutes}`;
 };
