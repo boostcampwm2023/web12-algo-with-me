a9ba9d3d99d288ebdad7caac1491bb52e8c28a91
dev2820 <second28200@naver.com>
2023-11-29 20:51:16 +0900

    refactor: 시뮬레이터 영역을 Competition에서 분리

---
+++ frontend/src/components/Simulation/Simulator.tsx
@@ -0,0 +1,82 @@
+import { css, cx } from '@style/css';
+
+import { HTMLAttributes, useContext } from 'react';
+
+import { SimulationInput, useSimulation } from '@/hooks/simulation';
+
+import { Modal } from '../Common';
+import { SimulationInputModal } from './SimulationInputModal';
+import { SimulationResultList } from './SimulationResultList';
+
+const ADD_SIMULATION = '테스트 케이스 추가하기';
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  code: string;
+}
+export function Simulator({ className, code, ...props }: Props) {
+  const simulation = useSimulation();
+  const modal = useContext(Modal.Context);
+
+  const handleSimulate = () => {
+    simulation.run(code);
+  };
+
+  const handleSimulationCancel = () => {
+    simulation.cancel();
+  };
+
+  const handleSaveSimulationInputs = (simulationInputs: SimulationInput[]) => {
+    simulation.changeInputs(simulationInputs);
+  };
+
+  function handleOpenModal() {
+    modal.open();
+  }
+
+  return (
+    <div className={cx(className)} {...props}>
+      <SimulationResultList resultList={simulation.results}></SimulationResultList>
+      <button className={execButtonStyle} onClick={handleOpenModal}>
+        {ADD_SIMULATION}
+      </button>
+      <ExecButton
+        isRunning={simulation.isRunning}
+        onExec={handleSimulate}
+        onCancel={handleSimulationCancel}
+      />
+      <SimulationInputModal
+        simulationInputs={simulation.inputs}
+        onSave={handleSaveSimulationInputs}
+      ></SimulationInputModal>
+    </div>
+  );
+}
+
+interface ExecButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  isRunning: boolean;
+  onExec: () => void;
+  onCancel: () => void;
+}
+
+const ExecButton = ({ isRunning, onExec, onCancel }: ExecButtonProps) => {
+  const RUN_SIMULATION = '테스트 실행';
+  const CANCEL_SIMULATION = '실행 취소';
+
+  if (isRunning) {
+    return (
+      <button className={execButtonStyle} onClick={onExec}>
+        {RUN_SIMULATION}
+      </button>
+    );
+  } else {
+    return (
+      <button className={execButtonStyle} onClick={onCancel}>
+        {CANCEL_SIMULATION}
+      </button>
+    );
+  }
+};
+
+const execButtonStyle = css({
+  color: 'black',
+});
--- frontend/src/pages/CompetitionPage.tsx
+++ frontend/src/pages/CompetitionPage.tsx
@@ -1,9 +1,9 @@
 import { css } from '@style/css';
 
-import { useContext, useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { HStack, Modal, VStack } from '@/components/Common';
+import { HStack, VStack } from '@/components/Common';
 import CompetitionHeader from '@/components/Competition/CompetitionHeader';
 import CompetitionProblemSelector from '@/components/Competition/CompetitionProblemSelector';
 import { CompetitionProvider } from '@/components/Competition/CompetitionProvider';
@@ -12,23 +12,15 @@ import Editor from '@/components/Editor/Editor';
 import { PageLayout } from '@/components/Layout/PageLayout';
 import { ProblemHeader } from '@/components/Problem/ProblemHeader';
 import ProblemViewer from '@/components/Problem/ProblemViewer';
-import { SimulationInputModal } from '@/components/Simulation/SimulationInputModal';
-import { SimulationResultList } from '@/components/Simulation/SimulationResultList';
+import { Simulator } from '@/components/Simulation';
 import { SubmissionResult } from '@/components/Submission';
 import { useCompetitionProblem } from '@/hooks/problem';
 import { useCompetitionProblemList } from '@/hooks/problem/useCompetitionProblemList';
-import { SimulationInput, useSimulation } from '@/hooks/simulation';
-
-const RUN_SIMULATION = '테스트 실행';
-const CANCEL_SIMULATION = '실행 취소';
 
 export default function CompetitionPage() {
   const { id } = useParams<{ id: string }>();
   const competitionId: number = id ? parseInt(id, 10) : -1;
   const [currentProblemIndex, setCurrentProblemIndex] = useState(0);
-  const modal = useContext(Modal.Context);
-
-  const simulation = useSimulation();
 
   const { problemList } = useCompetitionProblemList(competitionId);
 
@@ -51,22 +43,6 @@ export default function CompetitionPage() {
     setCode(newCode);
   };
 
-  const handleSimulate = () => {
-    simulation.run(code);
-  };
-
-  const handleSimulationCancel = () => {
-    simulation.cancel();
-  };
-
-  const handleSaveSimulationInputs = (simulationInputs: SimulationInput[]) => {
-    simulation.changeInputs(simulationInputs);
-  };
-
-  function handleOpenModal() {
-    modal.open();
-  }
-
   const problemIds = problemList.map((problem) => problem.id);
 
   return (
@@ -85,30 +61,16 @@ export default function CompetitionPage() {
             <ProblemViewer className={problemStyle} content={problem.content}></ProblemViewer>
             <HStack className={solutionStyle}>
               <Editor code={problem.solutionCode} onChangeCode={handleChangeCode}></Editor>
-              <SimulationResultList resultList={simulation.results}></SimulationResultList>
-              {simulation.isRunning ? (
-                <button className={execButtonStyle} onClick={handleSimulationCancel}>
-                  {CANCEL_SIMULATION}
-                </button>
-              ) : (
-                <button className={execButtonStyle} onClick={handleSimulate}>
-                  {RUN_SIMULATION}
-                </button>
-              )}
+              <section>
+                <Simulator code={code}></Simulator>
+              </section>
               <section>
                 <SubmissionResult></SubmissionResult>
                 <CompetitionSubmitButton
                   code={code}
                   problemId={currentProblem?.id}
                 ></CompetitionSubmitButton>
-                <button className={execButtonStyle} onClick={handleOpenModal}>
-                  테스트 케이스 추가하기
-                </button>
               </section>
-              <SimulationInputModal
-                simulationInputs={simulation.inputs}
-                onSave={handleSaveSimulationInputs}
-              ></SimulationInputModal>
             </HStack>
           </VStack>
         </div>
@@ -137,14 +99,11 @@ const problemStyle = css({
 });
 const solutionStyle = css({
   width: '50%',
+  overflow: 'auto',
 });
 
 const padVerticalStyle = css({
   paddingX: '1rem',
-});
-
-const execButtonStyle = css({
-  color: 'black',
 });
 
 const asideStyle = css({
