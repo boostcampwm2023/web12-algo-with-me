47639ff2f6c1c03f2fadf124b26bd8ebb01a2d1b
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-11-29 23:28:15 +0900

    refactor: 중복되는 코드 분리

---
+++ frontend/src/components/Dashboard/mockCompetitionData.ts
@@ -0,0 +1,33 @@
+export type Props = {
+  userList: Record<
+    string,
+    {
+      'Solved Problems': number;
+      'Total Time Spent': number;
+      Problems: Record<string, number | null>;
+    }
+  >;
+};
+
+export const mockCompetitionData: Props['userList'] = {
+  'tmp@gmail.com': {
+    'Solved Problems': 2,
+    'Total Time Spent': 19,
+    // eslint-disable-next-line quote-props
+    Problems: {
+      1: 12,
+      2: null,
+      4: 7,
+    },
+  },
+  'user2@gmail.com': {
+    'Solved Problems': 0,
+    'Total Time Spent': 0,
+    // eslint-disable-next-line quote-props
+    Problems: {
+      1: null,
+      2: null,
+      4: null,
+    },
+  },
+};
--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -4,44 +4,11 @@ import { useParams } from 'react-router-dom';
 
 import EnterCompetitionButton from '@/components/CompetitionDetail/Buttons/EnterCompetitionButton';
 import DashboardList from '@/components/Dashboard/DashboardList';
+import { mockCompetitionData } from '@/components/Dashboard/mockCompetitionData';
 import Header from '@/components/Header';
 import SocketTimer from '@/components/SocketTimer';
 import { useCompetition } from '@/hooks/competition';
 import { formatDate } from '@/utils/date';
-
-interface Props {
-  userList: Record<
-    string,
-    {
-      'Solved Problems': number;
-      'Total Time Spent': number;
-      Problems: Record<string, number | null>;
-    }
-  >;
-}
-
-const mockCompetitionData: Props['userList'] = {
-  'tmp@gmail.com': {
-    'Solved Problems': 2,
-    'Total Time Spent': 19,
-    // eslint-disable-next-line quote-props
-    Problems: {
-      1: 12,
-      2: null,
-      4: 7,
-    },
-  },
-  'user2@gmail.com': {
-    'Solved Problems': 0,
-    'Total Time Spent': 0,
-    // eslint-disable-next-line quote-props
-    Problems: {
-      1: null,
-      2: null,
-      4: null,
-    },
-  },
-};
 
 export default function DashboardPage() {
   const { id } = useParams<{ id: string }>();
@@ -53,8 +20,8 @@ export default function DashboardPage() {
   const formattedEndsAt = new Date(endsAt || '');
   const formattedStartsAtDate = formatDate(new Date(startsAt || ''), 'hh:mm');
   const formattedEndsAtDate = formatDate(new Date(endsAt || ''), 'hh:mm');
-  const COMPEITION_PING_TIME = 5 * 1000;
-  const COMPEITION_SOCKET_EVENT = 'ping';
+  const DASHBOARD_PING_TIME = 5 * 1000;
+  const DASHBOARD_SOCKET_EVENT = 'ping';
   const competitionSchedule = `시작: ${formattedStartsAtDate} 종료: ${formattedEndsAtDate}`;
 
   return (
@@ -70,8 +37,8 @@ export default function DashboardPage() {
               socket={socket.current}
               isConnected={isConnected}
               endsAt={new Date(endsAt)}
-              pingTime={COMPEITION_PING_TIME}
-              socketEvent={COMPEITION_SOCKET_EVENT}
+              pingTime={DASHBOARD_PING_TIME}
+              socketEvent={DASHBOARD_SOCKET_EVENT}
             />
           </div>
           <EnterCompetitionButton
