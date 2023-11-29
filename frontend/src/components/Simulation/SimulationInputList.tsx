594f10715768ca3ae54e1703d5171f436a89cd5b
youseock <mahwin7085@gmail.com>
2023-11-29 23:30:49 +0900

    feat: 테스트 케이스 추가하는 모달 구현

--- frontend/src/components/Simulation/SimulationInputList.tsx
+++ frontend/src/components/Simulation/SimulationInputList.tsx
@@ -4,27 +4,41 @@ import type { ChangeEvent, HTMLAttributes } from 'react';
 
 import type { SimulationInput } from '@/hooks/simulation';
 
+import type { InputChangeProps } from './SimulationInputModal';
+
 interface Props extends HTMLAttributes<HTMLUListElement> {
   inputList: SimulationInput[];
-  onChangeInput: (index: number, newInput: string) => void;
+  onChangeInput: ({ dataType, newInput, dataIndex }: InputChangeProps) => void;
+  onDeleteInput: (index: number) => void;
 }
 
 export function SimulationInputList(props: Props) {
-  const { inputList, onChangeInput, ...rest } = props;
+  const { inputList, onChangeInput, onDeleteInput, ...rest } = props;
 
-  const handleInputChange = (index: number, newInput: string) => {
-    onChangeInput(index, newInput);
+  const handleChange = ({ dataType, newInput, dataIndex }: InputChangeProps) => {
+    onChangeInput({ dataType, newInput, dataIndex });
   };
 
   return (
     <ul {...rest}>
-      {inputList.map(({ input, id }, index) => (
+      {inputList.map(({ input, expected, id, changable }, index) => (
         <li key={id} className={listStyle}>
           케이스 {index}:
           <SimulationInput
+            dataIndex={index + 1}
+            dataType="input"
             value={input}
-            onChange={(newInput) => handleInputChange(id, newInput)}
+            onChange={handleChange}
+            changable={changable as boolean}
           ></SimulationInput>
+          <SimulationInput
+            dataIndex={index + 1}
+            dataType="expected"
+            value={expected as string}
+            onChange={handleChange}
+            changable={changable as boolean}
+          ></SimulationInput>
+          {changable && <button onClick={() => onDeleteInput(id)}>-</button>}
         </li>
       ))}
     </ul>
@@ -33,24 +47,31 @@ export function SimulationInputList(props: Props) {
 
 interface SimulationInputProps {
   value: string;
-  onChange: (newInput: string) => void;
+  dataIndex: number;
+  dataType: string;
+  changable: boolean;
+  onChange: ({ dataType, newInput, dataIndex }: InputChangeProps) => void;
 }
 
 const SimulationInput = (props: SimulationInputProps) => {
-  const { value, onChange } = props;
+  const { value, onChange, dataIndex, dataType, changable } = props;
 
   const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
     const newInput = e.target.value;
     if (onChange) {
-      onChange(newInput);
+      onChange({ newInput, dataIndex, dataType });
     }
   };
-
+  if (!changable) {
+    return <input value={value} className={inputStyle} onChange={handleInput} disabled></input>;
+  }
   return <input value={value} className={inputStyle} onChange={handleInput}></input>;
 };
 
 const listStyle = css({
   marginBottom: '0.25rem',
+  display: 'flex',
+  gap: '0.5rem',
 });
 
 const inputStyle = css({
