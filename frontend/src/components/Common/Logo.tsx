de0b50073b968c0bbaf9f1b4362482d18a68820d
youseock <mahwin7085@gmail.com>
2023-12-03 16:56:11 +0900

    fix: 상대경로에서 절대경로로 변경

--- frontend/src/components/Common/Logo.tsx
+++ frontend/src/components/Common/Logo.tsx
@@ -4,5 +4,5 @@ interface Props {
 
 export default function Logo({ size }: Props) {
   // public이란 곳에 리소스 넣어두면 build시에 바로 밑에 생기기 때문에 ./으로 접근 가능합니다.
-  return <img src="./algo.png" alt="logo" width={size} height={size} />;
+  return <img src="/algo.png" alt="logo" width={size} height={size} />;
 }
