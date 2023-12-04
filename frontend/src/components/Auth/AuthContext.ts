3f01b2f298999a2a10e2bea81ad7dd7e0c8aa226
youseock <mahwin7085@gmail.com>
2023-12-04 17:08:39 +0900

    chore: ts-ignore 추가

--- frontend/src/components/Auth/AuthContext.ts
+++ frontend/src/components/Auth/AuthContext.ts
@@ -3,6 +3,7 @@ import { createContext } from 'react';
 const AuthContext = createContext({
   isLoggedin: false,
   email: '',
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   login: (email: string) => {},
   logout: () => {},
 });
