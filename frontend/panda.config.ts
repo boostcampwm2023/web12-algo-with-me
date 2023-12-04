fcec435f91348aeac76af5d7cafa1176fa546a07
dev2820 <second28200@naver.com>
2023-12-04 23:17:19 +0900

    refactor: Text 컴포넌트 추가

--- frontend/panda.config.ts
+++ frontend/panda.config.ts
@@ -31,6 +31,28 @@ export default defineConfig({
           DEFAULT: { value: '#455A64' },
         },
       },
+      fontSizes: {
+        display: {
+          lg: { value: '57px' },
+          md: { value: '45px' },
+          sm: { value: '36px' },
+        },
+        title: {
+          lg: { value: '22px' },
+          md: { value: '16px' },
+          sm: { value: '14px' },
+        },
+        body: {
+          lg: { value: '16px' },
+          md: { value: '14px' },
+          sm: { value: '12px' },
+        },
+        label: {
+          lg: { value: '14px' },
+          md: { value: '12px' },
+          sm: { value: '11px' },
+        },
+      },
     },
   },
 });
---
+++ frontend/src/components/Common/Text.tsx
@@ -0,0 +1,151 @@
+import { cva, cx } from '@style/css';
+
+import type { HTMLAttributes } from 'react';
+
+type TextType = 'display' | 'title' | 'body' | 'label';
+type SizeType = 'lg' | 'md' | 'sm';
+
+interface Props extends HTMLAttributes<HTMLSpanElement> {
+  type?: TextType;
+  size?: SizeType;
+  bold?: boolean;
+  underline?: boolean;
+}
+
+export function Text({
+  className,
+  children,
+  type = 'body',
+  size = 'md',
+  bold = false,
+  underline = false,
+  ...props
+}: Props) {
+  return (
+    <span
+      className={cx(className, fontStyle({ type, size }), style({ bold, underline }))}
+      {...props}
+    >
+      {children}
+    </span>
+  );
+}
+
+const style = cva({
+  base: {
+    fontWeight: 'normal',
+  },
+  variants: {
+    bold: {
+      true: {
+        fontWeight: 'bold',
+      },
+    },
+    underline: {
+      true: {
+        textDecoration: 'underline',
+      },
+    },
+  },
+});
+
+const fontStyle = cva({
+  compoundVariants: [
+    {
+      type: 'display',
+      size: 'lg',
+      css: {
+        fontSize: 'display.lg',
+        lineHeight: '64px',
+      },
+    },
+    {
+      type: 'display',
+      size: 'md',
+      css: {
+        fontSize: 'display.md',
+        lineHeight: '52px',
+      },
+    },
+    {
+      type: 'display',
+      size: 'sm',
+      css: {
+        fontSize: 'display.sm',
+        lineHeight: '44px',
+      },
+    },
+    {
+      type: 'display',
+      size: 'lg',
+      css: {
+        fontSize: 'display.lg',
+        lineHeight: '28px',
+      },
+    },
+    {
+      type: 'title',
+      size: 'md',
+      css: {
+        fontSize: 'title.md',
+        lineHeight: '24px',
+      },
+    },
+    {
+      type: 'title',
+      size: 'sm',
+      css: {
+        fontSize: 'title.sm',
+        lineHeight: '20px',
+      },
+    },
+    {
+      type: 'body',
+      size: 'lg',
+      css: {
+        fontSize: 'body.lg',
+        lineHeight: '24px',
+      },
+    },
+    {
+      type: 'body',
+      size: 'md',
+      css: {
+        fontSize: 'body.md',
+        lineHeight: '20px',
+      },
+    },
+    {
+      type: 'body',
+      size: 'sm',
+      css: {
+        fontSize: 'body.sm',
+        lineHeight: '16px',
+      },
+    },
+    {
+      type: 'label',
+      size: 'lg',
+      css: {
+        fontSize: 'label.lg',
+        lineHeight: '20px',
+      },
+    },
+    {
+      type: 'label',
+      size: 'md',
+      css: {
+        fontSize: 'label.md',
+        lineHeight: '16px',
+      },
+    },
+    {
+      type: 'label',
+      size: 'sm',
+      css: {
+        fontSize: 'label.sm',
+        lineHeight: '16px',
+      },
+    },
+  ],
+});
