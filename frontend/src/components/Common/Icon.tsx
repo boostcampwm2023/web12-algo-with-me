a4fe041715dbea0a09614df54d468d6244979772
dev2820 <second28200@naver.com>
2023-12-05 02:17:37 +0900

    refactor: Icon 컴포넌트 생성

---
+++ frontend/src/components/Common/Icon.tsx
@@ -0,0 +1,61 @@
+import { css, cva, cx } from '@style/css';
+
+import { HTMLAttributes } from 'react';
+
+import { type Theme } from '../types';
+import icons from '/icons.svg?url';
+
+type IconName = 'chevron-right' | 'check-round' | 'cancel-round' | 'spinner' | 'cancel';
+
+interface Props extends HTMLAttributes<HTMLSpanElement> {
+  size?: number;
+  color?: Theme;
+  name: IconName;
+}
+
+export function Icon({ className, size = 24, name = 'cancel', color = 'info', ...props }: Props) {
+  const iconPath = `${icons}#${name}`;
+
+  return (
+    <span className={cx(className, style, themeStyle({ color }))} {...props}>
+      <svg width={size} height={size} fill={color}>
+        <use href={iconPath}></use>
+      </svg>
+    </span>
+  );
+}
+
+Icon.ChevronRight = ({ ...props }: Omit<Props, 'name'>) => (
+  <Icon name="chevron-right" {...props}></Icon>
+);
+Icon.CheckRound = ({ ...props }: Omit<Props, 'name'>) => (
+  <Icon name="check-round" {...props}></Icon>
+);
+Icon.CancelRound = ({ ...props }: Omit<Props, 'name'>) => (
+  <Icon name="cancel-round" {...props}></Icon>
+);
+Icon.Spinner = ({ ...props }: Omit<Props, 'name'>) => <Icon name="spinner" {...props}></Icon>;
+Icon.Cancel = ({ ...props }: Omit<Props, 'name'>) => <Icon name="cancel" {...props}></Icon>;
+
+const style = css({
+  display: 'inline-block',
+});
+
+const themeStyle = cva({
+  variants: {
+    color: {
+      success: {
+        fill: 'alert.success',
+      },
+      danger: {
+        fill: 'alert.danger',
+      },
+      warning: {
+        fill: 'alert.warning',
+      },
+      info: {
+        fill: 'alert.info',
+      },
+    },
+  },
+});
