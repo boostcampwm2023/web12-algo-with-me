251626cb4126797b7d9c940237bda3dd5cbdda67
Yechan Lee <yechan24680@gmail.com>
2023-12-04 22:25:17 +0900

    chore: eslint 오류 제거

--- be/algo-with-me-api/src/app.module.ts
+++ be/algo-with-me-api/src/app.module.ts
@@ -5,8 +5,6 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from 'dotenv';
 import { WinstonModule } from 'nest-winston';
-import * as winston from 'winston';
-import * as winstonDaily from 'winston-daily-rotate-file';
 
 import { AuthModule } from './auth/auth.module';
 import { CompetitionModule } from './competition/competition.module';
