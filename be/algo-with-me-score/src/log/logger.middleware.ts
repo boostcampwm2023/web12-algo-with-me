0509b79a67f893405269bf3ed5f5e7a138c95dfa
Yechan Lee <yechan24680@gmail.com>
2023-12-04 22:50:24 +0900

    feat: score 서버 logger 적용

---
+++ be/algo-with-me-score/src/log/logger.middleware.ts
@@ -0,0 +1,17 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  logger = new Logger('HTTP');
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { method, originalUrl } = request;
+
+    response.on('finish', () => {
+      const { statusCode } = response;
+      this.logger.debug(`${method} ${originalUrl} ${statusCode}`);
+    });
+    next();
+  }
+}
