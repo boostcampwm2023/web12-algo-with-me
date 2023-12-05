6012a91aab7b75f6bb0cffc545385555f9f122fb
youseock <mahwin7085@gmail.com>
2023-12-05 17:41:03 +0900

    refactor: 필요없는 분기 제거

--- frontend/src/utils/localStorage/index.ts
+++ frontend/src/utils/localStorage/index.ts
@@ -1,9 +1,3 @@
-import { isDictionary } from '../type';
-
 export function save(key: string, origin: unknown) {
-  if (isDictionary(origin)) {
-    localStorage.setItem(key, JSON.stringify(origin));
-  } else {
-    localStorage.setItem(key, String(origin));
-  }
+  localStorage.setItem(key, JSON.stringify(origin));
 }
