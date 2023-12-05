250ddcbfaa3cf0de2a963715a5a0817d5971a37d
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-12-05 15:14:12 +0900

    refactor: 유틸함수 사용하도록 수정, 스코어가 테이블 뒤에 출력되도록 수정

--- frontend/src/components/Dashboard/DashboardTable.tsx
+++ frontend/src/components/Dashboard/DashboardTable.tsx
@@ -1,10 +1,12 @@
 import { css } from '@style/css';
 
 import { useParticipantDashboard } from '@/hooks/dashboard';
+import { range } from '@/utils/array';
+import { isNil } from '@/utils/type';
 
 export default function DashboardTable() {
   const { ranks, totalProblemCount, myRank } = useParticipantDashboard();
-  const problemCount = Array.from({ length: totalProblemCount }, (_, index) => index + 1);
+  const problemCount = range(1, totalProblemCount + 1);
 
   return (
     <table className={tableStyle}>
@@ -12,37 +14,37 @@ export default function DashboardTable() {
         <tr>
           <th className={thTdCommonStyle}>Rank</th>
           <th className={thTdCommonStyle}>User Id</th>
-          <th className={thTdCommonStyle}>Score</th>
           {problemCount.map((problemId) => (
             <th key={problemId} className={thTdCommonStyle}>
               problem{problemId}
             </th>
           ))}
+          <th className={thTdCommonStyle}>Score</th>
         </tr>
       </thead>
       <tbody>
-        {myRank !== null && (
+        {!isNil(myRank) && (
           <tr>
             <td className={thTdCommonStyle}>{myRank.rank}</td>
             <td className={thTdCommonStyle}>{myRank.email}</td>
-            <td className={thTdCommonStyle}>{myRank.score}</td>
             {problemCount.map((problemId) => (
               <td key={problemId} className={thTdCommonStyle}>
                 {myRank.problemDict[Number(problemId)] ?? '-'}
               </td>
             ))}
+            <td className={thTdCommonStyle}>{myRank.score}</td>
           </tr>
         )}
         {ranks.map((rank, index) => (
           <tr key={rank.email}>
             <td className={thTdCommonStyle}>{index + 1}</td>
             <td className={thTdCommonStyle}>{rank.email}</td>
-            <td className={thTdCommonStyle}>{rank.score}</td>
             {problemCount.map((problemId) => (
               <td key={problemId} className={thTdCommonStyle}>
                 {rank.problemDict[Number(problemId)] ?? '-'}
               </td>
             ))}
+            <td className={thTdCommonStyle}>{rank.score}</td>
           </tr>
         ))}
       </tbody>
