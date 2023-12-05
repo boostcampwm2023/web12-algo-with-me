ed4f013d2a13a6bcedc6ff51749c272eea94c43b
Yechan Lee <yechan24680@gmail.com>
2023-12-05 15:36:24 +0900

    fix: promise가 아닌 execSync timeout 쓰도록함

--- be/algo-with-me-docker/src/app.ts
+++ be/algo-with-me-docker/src/app.ts
@@ -6,39 +6,21 @@ const app = express();
 const PORT = 5000;
 const TIMEOUT_IN_MILLI = 10_000;
 
-function execute(cmd: string) {
-  return new Promise<boolean>((resolve, reject) => {
-    child_process.exec(cmd, (err, stdout, stderr) => {
-      if (err) {
-        reject(new Error(stderr));
-      }
-      resolve(true);
-    })
-  });
-}
-
-function getTimer(timeoutInMilli: number): Promise<never> {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {reject(new Error('TIMEOUT'));}, timeoutInMilli);
-  })
-}
-
 app.post('/:competitionId/:userId/:problemId/:testcaseId', (req, res) => {
   const {competitionId, userId, problemId, testcaseId} = req.params;
   // const result = execute(`/algo-with-me/node-sh/run.sh ${competitionId} ${userId} ${problemId}`); // docker instance용
   // const result = execute(`./node-sh/run.sh ${competitionId} ${userId} ${problemId}`); // local test용
   
   const responseJson = { result: '', competitionId, userId, problemId };
-  Promise.race([
-    execute(`/algo-with-me/node-sh/run.sh ${competitionId} ${userId} ${problemId} ${testcaseId}`),
-    getTimer(TIMEOUT_IN_MILLI),
-  ]).then(() => {
+  const command = `/algo-with-me/node-sh/run.sh ${competitionId} ${userId} ${problemId} ${testcaseId}`;
+  try {
+    child_process.execSync(command, { timeout: TIMEOUT_IN_MILLI });
     responseJson.result = 'SUCCESS';
-  }).catch((error) => {
-    responseJson.result = error.message;
-  }).finally(() => {
-    res.send(JSON.stringify(responseJson));
-  });
+  } catch (error) {
+    responseJson.result = error.code === 'ETIMEDOUT'? 'TIMEOUT' : error.code;
+  }
+  
+  res.send(JSON.stringify(responseJson));
 })
 
 app.listen(PORT, () => {
