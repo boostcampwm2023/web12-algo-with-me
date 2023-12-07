e5103d3a035865ce3393416990dd92e008e9a39a
Yechan Lee <yechan24680@gmail.com>
2023-12-07 18:13:27 +0900

    fix: 각각의 채점서버가 자신만의 도커서버를 갖도록 변경

--- be/algo-with-me-score/src/main.ts
+++ be/algo-with-me-score/src/main.ts
@@ -6,6 +6,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
-  await app.listen(process.env.SELF_PORT);
+  await app.listen(4000 + parseInt(process.env.SCORE_SERVER_ID));
 }
 bootstrap();
