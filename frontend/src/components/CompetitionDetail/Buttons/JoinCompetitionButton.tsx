d4f963671a20a6e4d253749de91d76934aeebcfc
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-12-07 23:22:55 +0900

    refactor: 테스트용 console 추가

--- frontend/src/components/CompetitionDetail/Buttons/JoinCompetitionButton.tsx
+++ frontend/src/components/CompetitionDetail/Buttons/JoinCompetitionButton.tsx
@@ -23,6 +23,7 @@ export default function JoinCompetitionButton(props: { id: number }) {
 
     const result = await joinCompetition(competitionData);
     alert(result);
+    console.log('test');
     navigate(0);
   };
   const competitionData: CompetitionApiData = {
