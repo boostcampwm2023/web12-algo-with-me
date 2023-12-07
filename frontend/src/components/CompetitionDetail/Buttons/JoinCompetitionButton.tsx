bb13776b8266bd3fa3b6e344d49d9e3a3f823350
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-12-07 10:57:02 +0900

    refactor: navigate 인자값 수정

--- frontend/src/components/CompetitionDetail/Buttons/JoinCompetitionButton.tsx
+++ frontend/src/components/CompetitionDetail/Buttons/JoinCompetitionButton.tsx
@@ -23,7 +23,7 @@ export default function JoinCompetitionButton(props: { id: number }) {
 
     const result = await joinCompetition(competitionData);
     alert(result);
-    navigate(`/contest/detail/${props.id}`);
+    navigate(0);
   };
   const competitionData: CompetitionApiData = {
     id: props.id,
