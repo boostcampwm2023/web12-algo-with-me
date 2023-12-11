2561adc05ad3ebdc9280f4a3ccb5bd5af6c3be2b
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-12-11 15:28:16 +0900

    refactor: 렌더링 조건문 수정 및 버퍼타임 추가

--- frontend/src/hooks/dashboard/useDashboardRenderState.ts
+++ frontend/src/hooks/dashboard/useDashboardRenderState.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 
 const TIME_INTERVAL = 1000;
+const ADDITIONAL_BUFFER_TIME = 3 * 1000;
 
 export function useDashboardRerenderState(endsAt: Date, bufferTimeAfterCompetitionEnd: Date) {
   const [shouldRenderLoading, setShouldRenderLoading] = useState(false);
@@ -9,17 +10,22 @@ export function useDashboardRerenderState(endsAt: Date, bufferTimeAfterCompetiti
     const intervalId = setInterval(() => {
       const currentDate = new Date();
 
-      if (currentDate >= endsAt && currentDate <= bufferTimeAfterCompetitionEnd) {
+      if (
+        currentDate >= endsAt &&
+        currentDate < new Date(bufferTimeAfterCompetitionEnd.getTime() + ADDITIONAL_BUFFER_TIME)
+      ) {
         setShouldRenderLoading(true);
-        return;
       }
 
-      setShouldRenderLoading(false);
-      return;
+      if (
+        currentDate >= new Date(bufferTimeAfterCompetitionEnd.getTime() + ADDITIONAL_BUFFER_TIME)
+      ) {
+        setShouldRenderLoading(false);
+      }
     }, TIME_INTERVAL);
 
     return () => clearInterval(intervalId);
-  }, [endsAt, shouldRenderLoading]);
+  }, [endsAt, shouldRenderLoading, bufferTimeAfterCompetitionEnd]);
 
   return shouldRenderLoading;
 }
