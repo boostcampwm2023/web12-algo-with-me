dd41e1647cbbae7f099531104fd66c7d0107f822
이우찬 <132538081+dmdmdkdkr@users.noreply.github.com>
2023-12-11 13:13:24 +0900

    feat: 대시보드 로딩 화면에 남은 시간이 출력되도록 수정

---
+++ frontend/src/hooks/dashboard/useCalculateRemainingTime.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+
+export function useCalculateRemainingTime(endsAt: Date) {
+  const [remainingTime, setRemainingTime] = useState<string>('');
+
+  useEffect(() => {
+    const endsAtDate = new Date(endsAt);
+
+    const calculateRemainingTime = () => {
+      const currentTime = new Date();
+      const timeDifference = endsAtDate.getTime() - currentTime.getTime();
+
+      if (timeDifference > 0) {
+        const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+        return `${minutes}분 ${seconds}초`;
+      }
+
+      return '';
+    };
+
+    const intervalId = setInterval(() => {
+      const newRemainingTime = calculateRemainingTime();
+      setRemainingTime(newRemainingTime);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [endsAt]);
+
+  return remainingTime;
+}
