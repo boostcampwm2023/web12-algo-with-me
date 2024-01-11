4443ce486be0cf78bc596278b576ffaea18a2e11
youseock <mahwin7085@gmail.com>
2024-01-11 17:08:03 +0900

    feat: 개발 모드에서 msw 설정 추가

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,5 +1,10 @@
 import { Outlet } from 'react-router-dom';
 
+import { worker } from './__mocks__/browser.ts';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 function App() {
   return <Outlet></Outlet>;
 }
