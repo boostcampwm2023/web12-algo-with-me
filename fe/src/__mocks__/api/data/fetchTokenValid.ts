81564098262734cd78c106287e3ee89bd2963faf
youseock <mahwin7085@gmail.com>
2024-01-12 13:17:51 +0900

    feat: node 환경msw test

---
+++ fe/src/__mocks__/api/data/fetchTokenValid.ts
@@ -0,0 +1,15 @@
+const response = {
+  correct: {
+    id: 2,
+    email: 'mahwin7085@gamil.com',
+    nickname: 'mahwin7085',
+    createdAt: '2021-10-13T07:50:00.000Z',
+    updatedAt: '2021-10-13T07:50:00.000Z',
+  },
+  inCorrect: {
+    message: 'Unauthorized',
+    statusCode: 401,
+  },
+};
+
+export default response;
