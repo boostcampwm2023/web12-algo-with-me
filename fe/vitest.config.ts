094b1034e4fff32decab0f16a407cd00892ee3de
youseock <mahwin7085@gmail.com>
2024-01-12 13:15:51 +0900

    chore: vitest path alias 설정, react render 테스트 라이브러리 추가

--- fe/vitest.config.ts
+++ fe/vitest.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
@@ -5,5 +6,10 @@ export default defineConfig({
     globals: true,
     root: __dirname,
     setupFiles: ['./vitest.setup.ts'],
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
