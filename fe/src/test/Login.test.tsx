7d34e201bc90c6cd4271d1a2150eb18e0716fad8
youseock <mahwin7085@gmail.com>
2024-02-15 15:45:54 +0900

    chore: 클릭에 따른 로그인 로직 테스팅

--- fe/src/test/Login.test.tsx
+++ fe/src/test/Login.test.tsx
@@ -5,8 +5,8 @@ import Header from '@/components/Header';
 
 import '@/utils/test/localStorage';
 import '@testing-library/jest-dom';
-import { cleanup, render } from '@testing-library/react';
-import { beforeEach, describe, expect, test, vi } from 'vitest';
+import { cleanup, fireEvent, render } from '@testing-library/react';
+import { beforeEach, describe, expect, it, test, vi } from 'vitest';
 
 const CORRECT_TOKEN = 'correct';
 const INCORRECT_TOKEN = 'inCorrect';
@@ -26,9 +26,11 @@ const useLocationReturn = (token: string) => ({
 
 type RRD = Record<string, unknown>;
 
+const usedNavigate = vi.fn();
+
 vi.mock('react-router-dom', async () => {
   const originRRD = (await vi.importActual('react-router-dom')) as RRD;
-  return { ...originRRD, useLocation: vi.fn(), useNavigate: vi.fn(), Link: vi.fn() };
+  return { ...originRRD, useLocation: vi.fn(), useNavigate: () => usedNavigate, Link: vi.fn() };
 });
 
 describe('Header 컴포넌트 검증 with 올바른 토큰', () => {
@@ -85,5 +87,42 @@ describe('Header 컴포넌트 검증 with 잘못된 LocalStorage 값', () => {
       </AuthProvider>,
     );
     expect(await findByText(/로그인/i)).toBeInTheDocument();
+  });
+});
+
+describe('Header 컴포넌트 검증 로그인 버튼 클릭', () => {
+  test('useNavigate가 /login 으로 routing 한다.', async () => {
+    // @ts-expect-error ts-ignore
+    vi.spyOn(reactRouterDom, 'useLocation').mockReturnValue('');
+
+    const { findByText } = render(
+      <AuthProvider>
+        <Header />
+      </AuthProvider>,
+    );
+    const loginButton = await findByText(/로그인/i);
+    fireEvent.click(loginButton);
+    expect(usedNavigate).toHaveBeenCalledWith('/login');
+  });
+});
+
+describe('Header 컴포넌트 검증 로그아웃 버튼 클릭', () => {
+  it('저장된 유저 정보가 없어지고, 로그인 버튼이 렌더링된다.', async () => {
+    vi.spyOn(reactRouterDom, 'useLocation').mockReturnValue(useLocationReturn(CORRECT_TOKEN));
+
+    const { findByText } = render(
+      <AuthProvider>
+        <Header />
+      </AuthProvider>,
+    );
+
+    const logOutButton = await findByText(/로그아웃/i);
+    test('로그아웃 버튼을 누르기 전에는 accessToken이 존재한다.', () => {
+      expect(localStorage.getItem('accessToken')).toBe('correct');
+    });
+    test('로그아웃 버튼을 누르 후 accessToken은 null이다.', () => {
+      fireEvent.click(logOutButton);
+      expect(localStorage.getItem('accessToken')).toBe(null);
+    });
   });
 });
