72a8f4530a7326182bc52e71656f0b5a74e54587
Yechan Lee <yechan24680@gmail.com>
2024-02-27 16:21:18 +0900

    feat: 솔루션 코드 만드는 로직 작성

---
+++ be/algo-with-me-api/src/competition/language.enums.ts
@@ -0,0 +1,45 @@
+export const Language = {
+  JavaScript: 'JavaScript',
+  Python3: 'Python3',
+} as const;
+
+interface ILanguageMetadata {
+  oneLineComment: string;
+  indent: string;
+  integerDefaultValue: string;
+  stringDefaultValue: string;
+  booleanDefaultValue: string;
+}
+export const LanguagesMetadata: { [key in keyof typeof Language]: ILanguageMetadata } = {
+  JavaScript: {
+    oneLineComment: '//',
+    indent: '  ',
+    integerDefaultValue: '0',
+    stringDefaultValue: "''",
+    booleanDefaultValue: 'true',
+  } as ILanguageMetadata,
+  Python3: {
+    oneLineComment: '//',
+    indent: '    ',
+    integerDefaultValue: '0',
+    stringDefaultValue: "''",
+    booleanDefaultValue: 'True',
+  } as ILanguageMetadata,
+} as const;
+
+export type ParameterDataType =
+  | 'integer'
+  | 'string'
+  | 'boolean'
+  | 'integer[]'
+  | 'string[]'
+  | 'boolean[]'
+  | 'integer[][]'
+  | 'string[][]'
+  | 'boolean[][]'
+  | string;
+
+export interface IParameter {
+  name: string;
+  datatype: ParameterDataType;
+}
--- be/algo-with-me-api/src/competition/services/problem.service.ts
+++ be/algo-with-me-api/src/competition/services/problem.service.ts
@@ -9,6 +9,7 @@ import { CreateProblemDto } from '../dto/create-problem.dto';
 import { ProblemResponseDto } from '../dto/problem.response.dto';
 import { ProblemSimpleResponseDto } from '../dto/problem.simple.response.dto';
 import { Problem } from '../entities/problem.entity';
+import { Language, IParameter, LanguagesMetadata } from '../language.enums';
 
 @Injectable()
 export class ProblemService {
@@ -40,6 +41,70 @@ export class ProblemService {
   async getProblemTestcaseNum(id: number) {
     const problem = await this.problemRepository.findOneBy({ id });
     return problem.testcaseNum;
+  }
+
+  generateSolutionCode(
+    language: keyof typeof Language,
+    inputs: IParameter[],
+    output: IParameter,
+  ): string {
+    const inputParams = this.getInputParams(inputs);
+    const parameterDescriptions = this.getParameterDescriptions(inputs, output, language);
+    const returnStatement = this.getSolutionCodeReturnStatement(output, language);
+    switch (language) {
+      case Language.JavaScript:
+        return `function solution(${inputParams}) {\n${parameterDescriptions}\n${returnStatement}\n}\n`;
+      case Language.Python3:
+        return `def solution(${inputParams}):\n${parameterDescriptions}\n${returnStatement}\n`;
+      default:
+        throw new Error(`${language} 언어는 지원하지 않는 프로그래밍 언어입니다`);
+    }
+  }
+
+  private getInputParams(inputs: IParameter[]): string {
+    return inputs.map((x) => x.name).join(', ');
+  }
+
+  private getParameterDescriptions(
+    inputs: IParameter[],
+    output: IParameter,
+    language: keyof typeof Language,
+  ) {
+    const languageMetadata = LanguagesMetadata[language];
+    const parameters = inputs.concat(output);
+    return parameters
+      .map(
+        (x) =>
+          `${languageMetadata.indent}${languageMetadata.oneLineComment} ${x.name}: ${x.datatype}`,
+      )
+      .join(`\n`);
+  }
+
+  private getSolutionCodeReturnStatement(
+    output: IParameter,
+    language: keyof typeof Language,
+  ): string {
+    let result = this.getDefaultReturnValue(output, language);
+    for (const char of output.datatype) {
+      if (char === '[') {
+        result = `[${result}]`;
+      }
+    }
+    result = `${LanguagesMetadata[language].indent}return ${result}`;
+    return result;
+  }
+
+  private getDefaultReturnValue(output: IParameter, language: keyof typeof Language): string {
+    let result: string;
+    const languageMetadata = LanguagesMetadata[language];
+    if (output.datatype.startsWith('integer')) {
+      result = languageMetadata.integerDefaultValue;
+    } else if (output.datatype.startsWith('string')) {
+      result = languageMetadata.stringDefaultValue;
+    } else if (output.datatype.startsWith('boolean')) {
+      result = languageMetadata.booleanDefaultValue;
+    }
+    return result;
   }
 
   // update(id: number, updateCompetitionDto: UpdateCompetitionDto) {
