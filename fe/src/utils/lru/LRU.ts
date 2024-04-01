7b384b7ae5371cc841ba6ac163388137cc62c36a
youseock <mahwin7085@gmail.com>
2024-04-02 02:23:30 +0900

    feat: 캐싱에 필요한 LRU 클래스

--- fe/src/utils/lru/LRU.ts
+++ fe/src/utils/lru/LRU.ts
@@ -1,31 +1,30 @@
 import { DoublyLinkedList, Node } from './DoblyLinkedList';
+import { type Key } from './type';
 
-type Key = string | number | symbol;
-
-export class LRU {
+export class LRU<T> {
   maxSize: number;
-  map: Map<Key, Node>;
-  list: DoublyLinkedList;
+  map: Map<Key, Node<T>>;
+  list: DoublyLinkedList<T>;
   constructor(maxSize: number) {
     this.maxSize = maxSize;
     this.map = new Map();
     this.list = new DoublyLinkedList();
   }
+
   get(key: Key) {
-    if (!this.map.has(key)) return false;
+    if (!this.map.has(key)) return null;
 
     // 현재 값 제일 최신으로 업데이트
-    const node = this.map.get(key) as Node;
+    const node = this.map.get(key) as Node<T>;
     this.list.swapToHead(node);
     return node.value;
   }
 
-  set(key: Key, value: unknown) {
+  set(key: Key, value: T) {
     const newNode = new Node(key, value);
-
     if (this.map.size === this.maxSize) {
       // 가장 오래된 값 제거
-      const lastNode = this.list.tail as Node;
+      const lastNode = this.list.tail as Node<T>;
       this.list.remove(lastNode);
       this.map.delete(lastNode.key);
     }
@@ -33,9 +32,9 @@ export class LRU {
     this.list.insertHead(newNode);
   }
   toMapString() {
-    const info: string[] = [];
+    const info: Key[] = [];
     this.map.forEach((_, key) => {
-      info.push(key as string);
+      info.push(key);
     });
     return info.join(',');
   }
